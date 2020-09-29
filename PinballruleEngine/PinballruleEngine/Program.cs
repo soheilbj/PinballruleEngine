b5ce3c078b6befc6bba77b647492996eab254705
S.Bijavar <S.Bijavar@PC-01>
2020-09-29 09:03:08 +0330

    add to repo first commit

---
+++ PinballruleEngine/PinballruleEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PinballruleEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
